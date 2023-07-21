efe7b2c92d9e408d39df62a9959b9825eb5c1166
Victor Nardello <victordrum17@gmail.com>
2023-07-20 22:33:24 -0300

    Refactoring and cleaning up classes

--- src/main/java/br/com/orderhere/service/GenerateService.java
+++ src/main/java/br/com/orderhere/service/GenerateService.java
@@ -16,7 +16,8 @@ public class GenerateService {
     public String generateDescription() {
         StringBuilder stringBuilder = new StringBuilder();
         String description = "Eu quero ";
-        String[] foods = {"Cookies", "Mil shake", "Esfiha", "Macarrão", "Marmita G", "Marmita M", "Marmita P", "Açai",
+        String[] foods = {
+                "Cookies", "Mil shake", "Esfiha", "Macarrão", "Marmita G", "Marmita M", "Marmita P", "Açai",
                 "Lanche", "Macarronada", "Pizza Familia", "Pizza Pequena", "Churrasco", "Strogonoff", "Pudim", "Pavê", "Milho Verde",
                 "Pastel", "Lasanha", "Hot Dog", "Comida Japonesa", "Comida Árabe", "Petit Gateau", "Chocolate", "Salgado assado",
                 "Temaki", "Tilápia", "Salmão grelhado", "Carne de panela", "Amendoim", "Miojo", "Carbonara", "Torta de frango",
@@ -25,8 +26,8 @@ public class GenerateService {
                 "Frango empanado", "Parmegiana", "Feijoada", "Galinhada", "Torresmo com cerveja", "Pamonha", "Pão de queijo"
         };
 
-        int index = ThreadLocalRandom.current().nextInt(foods.length);
-        String randomFoods = foods[index];
+        int indexFood = ThreadLocalRandom.current().nextInt(foods.length);
+        String randomFoods = foods[indexFood];
 
         return String.valueOf(stringBuilder.append(description).append(randomFoods));
     }
@@ -48,24 +49,23 @@ public class GenerateService {
     };
 
     public String generateNickName() {
-        String[] nickNames = {"James", "Miguelito", "Arthero", "Gael", "Théo", "Heitor", "Ravi", "Davizin", "Maitê", "Liz",
+        String[] nickNames = {
+                "James", "Miguelito", "Arthero", "Gael", "Théo", "Heitor", "Ravi", "Davizin", "Maitê", "Liz",
                 "Cecília", "Bernardo", "Noah", "Bielzinho", "Helena", "Alice", "Laura", "Maria Alice", "Sophia", "Manu",
                 "Belinha", "Causador", "Pudinzinho", "Flora", "Lua", "Baunilha", "Smurf", "Alecrim dourado", "Amendoim",
                 "Sereia", "Chica", "Amora", "Sugar", "Honey", "Jimmy", "Turner", "Johnson's Baby", "Musa", "Lady",
-                "Aqua", "Floquinho", "Mustache", "Luna", "Caracol", "Vanilla", "Aurora", "Batman", "Butterfly"};
+                "Aqua", "Floquinho", "Mustache", "Luna", "Caracol", "Vanilla", "Aurora", "Batman", "Butterfly"
+        };
 
-        int index = ThreadLocalRandom.current().nextInt(nickNames.length);
-        String randomNickNames = nickNames[index];
+        int indexNicksNames = ThreadLocalRandom.current().nextInt(nickNames.length);
 
-        return randomNickNames;
+        return nickNames[indexNicksNames];
     }
 
     public BigDecimal generateOrderPrice() {
         Random random = new Random();
         double randomMoney = random.nextInt(51) + 10;
 
-        BigDecimal returnPrice = BigDecimal.valueOf(randomMoney).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-        return returnPrice;
+        return new BigDecimal(randomMoney).setScale(2, BigDecimal.ROUND_HALF_EVEN);
     }
 }
